af81dab269c4e565a55faa6be89d84de9e8f0ccc
AyaHechaichi <99262836+AyaHechaichi@users.noreply.github.com>
2022-02-10 23:03:29 +0100

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,26 +1,46 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
 
 public class DBConnection {
 	   
 		String BDD = "nomBD";
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
-
-	   
-	    public DBConnection() throws SQLException {
+		static String user = "root";
+		static String passwd = "";
+		// turning the instance to static 
+	    static Connection conn;
+	    // and the constructor to private so we can't make instances 
+	    private DBConnection() throws SQLException {
 			conn=DriverManager.getConnection(url, user,passwd);
 		}
 
-	    
-	    public Connection getConn() {
+	    public static Connection getConn() {
+	    	try {
+	    		if (conn== null ) {
+	    			Class.forName("com.mysql.jdbc.Driver");
+	    			conn= DriverManager.getConnection("jdbc:mysql://localhost/tp2Archi","root","");
+	    			java.sql.Statement stmt = conn.createStatement();	 
+	    			String sql = "Select * from dbconnction where userName='"+user.toString()+"'abd Password='"+passwd.toString()+"'";
+	    			ResultSet rs = stmt.executeQuery(sql);
+	    			if (rs.next())
+					{
+						System.out.println("connecté");
+					}else {
+						System.out.println("nom d'utilisateur ou mot de passe incorrecte");
+					}
+					conn.close();
+	    		}
+	    			
+	    	}catch (Exception e) {
+	    		e.printStackTrace();
+	    	}
 			return conn;
 		}
 
-
-		
 	
 }
+
